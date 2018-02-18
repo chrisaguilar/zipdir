f44df9f41d0e8cd9a6cdec5c99f260a787a29afa
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-02-18 15:53:44 -0500

    Added input validation function & unit test

---
+++ src/validate-input.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as util from 'util';
+
+const lstat = util.promisify(fs.lstat);
+
+export async function validateInput(dirs: string[], ...args: any[]): Promise<void> {
+    if (dirs === undefined) throw new Error('Expected at least one argument, got 0');
+    if (!Array.isArray(dirs)) throw new Error(`Expected array, got ${typeof dirs}`);
+    if (!dirs.every(e => typeof e === 'string')) throw new Error('Expected an array of strings');
+    if (dirs.length < 2) throw new Error('Expected an array of at least length 2');
+    if (args.length > 0) throw new Error(`Expected one argument, got ${args.length + 1}`);
+
+    for (const dir of dirs) {
+        const directory = path.resolve(process.cwd(), dir);
+
+        const stat = await lstat(directory).catch(_ => {
+            throw new Error(`"${directory}" is not a file or directory`);
+        });
+
+        if (!stat.isDirectory()) throw new Error(`"${dir}" is not a directory`);
+    }
+}
---
+++ test/src/validate-input.test.ts
@@ -0,0 +1,108 @@
+import * as path from 'path';
+
+import { expect } from 'chai';
+import * as fs from 'fs-extra';
+
+import { validateInput } from '~zipdir/validate-input';
+
+describe('validate-input', () => {
+    const testDir = path.join(process.cwd(), 'testDir');
+    const dir1 = 'testDir/a';
+    const dir2 = './testDir/b';
+    const dir3 = 'testDir/c';
+    const file1 = 'testDir/d';
+
+    before(async () => {
+        await fs.mkdir(testDir);
+        await fs.mkdir(dir1);
+        await fs.mkdir(dir2);
+        await fs.mkdir(dir3);
+        await fs.ensureFile(file1);
+    });
+
+    after(async () => await fs.remove(testDir));
+
+    it('should throw if not given any arguments', async () => {
+        await validateInput().catch(e => {
+            expect(e.message).to.equal('Expected at least one argument, got 0');
+        });
+    });
+
+    it('should throw if not given an array', async () => {
+        const testCases = [true, 'foobar', 12, {}, null, function() {}];
+
+        for (const testCase of testCases) {
+            await validateInput(testCase as any).catch(e => {
+                expect(e.message).to.equal(`Expected array, got ${typeof testCase}`);
+            });
+        }
+    });
+
+    it('should throw if not given an array of strings', async () => {
+        const testCases = [[true, false, true], [12, 13, 14], [null, undefined, null]];
+
+        for (const testCase of testCases) {
+            await validateInput(testCase as any).catch(e => {
+                expect(e.message).to.equal('Expected an array of strings');
+            });
+        }
+    });
+
+    it('should throw if not given an array of length >= 2', async () => {
+        const testCases = [[], ['a']];
+        for (const testCase of testCases) {
+            await validateInput(testCase as any).catch(e => {
+                expect(e.message).to.equal('Expected an array of at least length 2');
+            });
+        }
+    });
+
+    it('should throw if given more than one argument', async () => {
+        const testCases = [[['a', 'b', 'c'], 'd']];
+
+        for (const testCase of testCases) {
+            await validateInput(...(testCase as any)).catch(e => {
+                expect(e.message).to.equal(`Expected one argument, got ${testCase.length}`);
+            });
+        }
+    });
+
+    it('should throw if any element in the given array is not a file or directory', async () => {
+        const testCases = [
+            ['a', 'b', 'c', 'd'],
+            [dir1, 'b', 'c', 'd'],
+            [dir1, dir2, 'c', 'd'],
+            [dir1, dir2, dir3, 'd']
+            // [dir1, dir2, dir3, file1]
+        ];
+
+        for (const [i, testCase] of Object.entries(testCases)) {
+            await validateInput(testCase as any).catch(e => {
+                expect(e.message).to.equal(`"${path.resolve(testCase[+i])}" is not a file or directory`);
+            });
+        }
+    });
+
+    it('should throw if any element in the given array is not a directory', async () => {
+        const testCases = [
+            [dir1, dir2, dir3, file1],
+            [dir1, dir2, file1, dir3],
+            [dir1, file1, dir2, dir3],
+            [file1, dir1, dir2, dir3]
+        ];
+
+        for (const testCase of testCases) {
+            await validateInput(testCase as any).catch(e => {
+                expect(e.message).to.equal(`"${file1}" is not a directory`);
+            });
+        }
+    });
+
+    it('should not throw if each element in the given array resolves to a valid path', async () => {
+        try {
+            await validateInput([dir1, dir2, dir3]);
+        } catch (e) {
+            expect(e).to.not.exist;
+        }
+    });
+});
